e44f2325ff85db1804353cd44a35e2d9add9b8cd
Nguyen Dinh Duy Thong <nguyendinhduythong@gmail.com>
2022-01-02 21:22:12 +0700

    Scan Package

--- TestingOnline/src/main/java/backend/testingonline/TestingOnlineApplication.java
+++ TestingOnline/src/main/java/backend/testingonline/TestingOnlineApplication.java
@@ -2,8 +2,10 @@ package backend.testingonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"backend.testingonline"})
+@ComponentScan(basePackages = {"backend.testingonline"})
 public class TestingOnlineApplication {
 
 	public static void main(String[] args) {
