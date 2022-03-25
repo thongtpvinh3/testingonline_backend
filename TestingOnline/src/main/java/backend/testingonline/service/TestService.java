1e229a23f418489297a95880b381a68553d5fc7b
Nguyen Dinh Duy Thong <nguyendinhduythong@gmail.com>
2022-03-25 13:33:52 +0700

    redis config

--- TestingOnline/src/main/java/backend/testingonline/service/TestService.java
+++ TestingOnline/src/main/java/backend/testingonline/service/TestService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
 
 import backend.testingonline.model.Candidate;
 import backend.testingonline.model.Question;
